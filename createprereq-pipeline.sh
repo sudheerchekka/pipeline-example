bfb34acf58615a98b828d0557c592b1478d54d51
Sudheer Chekka <sudheer.chekka@gmail.com>
2017-08-02 02:53:00 -0700

    added projects

--- createprereq-pipeline.sh
+++ createprereq-pipeline.sh
@@ -1,1 +1,19 @@
+#!/bin/sh
+oc new-project cicd
+
+#development
+oc new-project development
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n development
+
+oc new-app nodejs~https://github.com/sudheerchekka/nodejs-welcome.git --name=welcome
+oc expose svc welcome
+
+#testing
+oc new-project testing
+oc policy add-role-to-user edit system:serviceaccount:cicd:jenkins -n testing
+oc policy add-role-to-group system:image-puller system:serviceaccounts:testing -n development
+
+oc create deploymentconfig welcome --image="$1":5000/development/welcome:promoteToQA
+oc expose dc welcome --port=8080
+oc expose svc welcome
 
